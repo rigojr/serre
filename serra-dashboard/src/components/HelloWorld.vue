e41137da5908ce56724968f789fe19f358c631e3
José Rigoberto Salas Chacón <rigojunnior@gmail.com>
2023-04-29 19:57:42 -0500

    add tailwindcss and daisyui

--- serra-dashboard/src/components/HelloWorld.vue
+++ serra-dashboard/src/components/HelloWorld.vue
@@ -10,8 +10,8 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
+    <button class="btn btn-primary" @click="count++">count is {{ count }}</button>
+    <p class="underline">
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
